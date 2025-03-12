265ca8fadd481a3572046cbabbe721883af3c753
yosuke ota <otameshiyo23@gmail.com>
2025-03-12 15:10:26 +0900

    update

--- playground/eslint.config.mjs
+++ playground/eslint.config.mjs
@@ -1,3 +1,12 @@
 import js from "@eslint/js";
 import moduleInterop from "eslint-plugin-module-interop";
-export default [js.configs.recommended, moduleInterop.configs.recommended];
+export default [
+  js.configs.recommended,
+  moduleInterop.configs.recommended,
+  {
+    rules: {
+      "module-interop/no-import-cjs": "error",
+      "module-interop/no-require-esm": "error",
+    },
+  },
+];
