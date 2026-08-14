283effc476671696226ec2c15d6622fe1b82feb1
yosuke ota <otameshiyo23@gmail.com>
2026-08-14 09:00:26 +0900

    fix: support Changesets release plan v5

--- tools/lib/changesets-util.ts
+++ tools/lib/changesets-util.ts
@@ -1,4 +1,3 @@
-import getReleasePlan from "@changesets/get-release-plan";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -6,9 +5,14 @@ const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** Get new version string from changesets */
 export async function getNewVersion(): Promise<string> {
+  const { getReleasePlan } = await import("@changesets/get-release-plan");
   const releasePlan = await getReleasePlan(path.resolve(dirname, "../.."));
 
-  return releasePlan.releases.find(
+  const release = releasePlan.releases.find(
     ({ name }) => name === "eslint-plugin-module-interop",
-  )!.newVersion;
+  );
+  if (!release || release.type === "none") {
+    throw new Error("Could not find a versioned release.");
+  }
+  return release.newVersion;
 }
